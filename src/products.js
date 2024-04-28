099f2471133a6a28082df8a3fe852d4286fc7938
to-mrR0w <marci.sch.2006@gmail.com>
2024-04-28 19:51:41 +0200

    New Price 39.99

--- src/products.js
+++ src/products.js
@@ -1,70 +1,70 @@
 export const DATA = [
-  {
-    id: 1,
-    name: "EasyCharge",
-    price: 45.99,
-    productImg: "/Product.jpeg",
-    productPay: "https://buy.stripe.com/bIYg2me9UafM3OE5kl",
-    disabledd: false,
-    // productPay: "https://buy.stripe.com/test_fZe4hE8CydEw8U06oq",
-  },
+    {
+        id: 1,
+        name: "EasyCharge",
+        price: 39.99,
+        productImg: "/Product.jpeg",
+        productPay: "https://buy.stripe.com/bIYdUed5QfA6dpefZ0",
+        disabledd: false,
+        // productPay: "https://buy.stripe.com/test_fZe4hE8CydEw8U06oq",
+    },
 ];
 {
-  /*export const dataProdImg = [
-  {
-    id: 2,
-    name: productImg2,
-    productImg: "/productImg2.jpg",
-  },
-  {
-    id: 3,
-    name: productImg3,
-    productImg: "/productImg3.jpg",
-  },
-];*/
+    /*export const dataProdImg = [
+    {
+      id: 2,
+      name: productImg2,
+      productImg: "/productImg2.jpg",
+    },
+    {
+      id: 3,
+      name: productImg3,
+      productImg: "/productImg3.jpg",
+    },
+  ];*/
 }
 export const FOUNDERS = [
-  {
-    class: "a",
-    id: 1,
-    name: "Radic Daniel",
-    image: "/5.jpg",
-    job: "Sales and Social Media",
-  },
-  {
-    class: "b",
-    id: 2,
-    name: "Karner Laurens",
-    image: "/4.jpg",
-    job: "Production",
-  },
-  {
-    class: "c",
+    {
+        class: "a",
+        id: 1,
+        name: "Radic Daniel",
+        image: "/5.jpg",
+        job: "Sales and Social Media",
+    },
+    {
+        class: "b",
+        id: 2,
+        name: "Karner Laurens",
+        image: "/4.jpg",
+        job: "Production",
+    },
+    {
+        class: "c",
 
-    id: 4,
-    name: "Weninger David",
-    image: "/1.jpg",
-    job: "CEO",
-  },
-  {
-    class: "d",
+        id: 4,
+        name: "Weninger David",
+        image: "/1.jpg",
+        job: "CEO",
+    },
+    {
+        class: "d",
 
-    id: 3,
-    name: "Schütz Tobias",
-    image: "/2.jpg",
-    job: "Marketing and CO-CEO",
-  },
-  {
-    class: "e",
+        id: 3,
+        name: "Schütz Tobias",
+        image: "/2.jpg",
+        job: "Marketing and CO-CEO",
+    },
+    {
+        class: "e",
 
-    id: 5,
-    name: "Schmiedjell Marcel",
-    image: "/3.jpg",
-    job: "Finance and IT",
-  },
+        id: 5,
+        name: "Schmiedjell Marcel",
+        image: "/3.jpg",
+        job: "Finance and IT",
+    },
 ];
 {
-  /* <li className="text-center">
+/* <li className="text-center">
 <img src="/Kevin Hart.PNG" alt="PB-M" />
 Schmiedjell Marcel
 </li>
